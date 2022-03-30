18cede69d6b2c4da6f8165614984f7fcb70fc77b
yugogamer <57806524+yugogamer@users.noreply.github.com>
2022-03-30 11:49:58 +0200

    [func] configure actix web server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::{HttpServer, App};
+use std::time::Duration;
+
+use actix_web::{HttpServer, App, web};
 
 use crate::{utils::{configuration::Configuration, database::connection}, controller::base::status};
 
@@ -12,13 +14,20 @@ async fn main() -> Result<(), tokio_postgres::Error>{
 
     let connection = connection(&config).await?;
     
-    HttpServer::new(move ||{
+    let result = HttpServer::new(move ||{
         App::new()
         .app_data(connection.clone())
         .service(status)
+        .service(
+            web::scope("/api").service(web::scope("/v1"))
+        )
     })
+    .keep_alive(Duration::from_secs(120))
     .bind((config.addresse.as_str(), config.port)).expect("err : no dispnible binding")
     .run().await;
 
+    if let Err(err) = result {
+        eprintln!("{}", err);
+    }
     Ok(())
 }
