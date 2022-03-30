703d6c805100c1bbb166fb6dd46ebfa29dad6d56
yugogamer <57806524+yugogamer@users.noreply.github.com>
2022-03-30 16:49:37 +0200

    [fix] fix add_user in service

--- src/service/user.rs
+++ src/service/user.rs
@@ -14,9 +14,12 @@ pub async fn get_user(conn : &Client, id : i32) -> Result<User, Box<dyn Error>>{
 }
 
 pub async fn add_user(conn : &Client, user : &UserInsertion) -> Result<User, Box<dyn Error>>{
-    let row = conn.query_one("INSERT INTO accounts (username, first_name, last_name, abreviate_name, mail) VALUES ($1, $2, $3, $4, $5)", &[&user.username, &user.first_name, &user.last_name, &user.abreviate_name, &user.mail]).await?;
+    let row = conn.query("INSERT INTO accounts (username, first_name, last_name, abreviate_name, mail) VALUES ($1, $2, $3, $4, $5) RETURNING *", &[&user.username, &user.first_name, &user.last_name, &user.abreviate_name, &user.mail]).await?;
     
-    let user = User::from_row(row)?;
+    for row in row {
+        let user = User::from_row(row)?;
+        return Ok(user);
+    }
 
-    return Ok(user);
+    return Err("user not inserted".into());
 }
