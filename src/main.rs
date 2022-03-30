931fe78b0823a7aca6673162a1d7a6ff3815f9e3
yugogamer <57806524+yugogamer@users.noreply.github.com>
2022-03-30 10:41:10 +0200

    [func] updating database to use a pool

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use crate::utils::{configuration::Configuration, database::connection};
+use actix_web::{HttpServer, App};
+
+use crate::{utils::{configuration::Configuration, database::connection}, controller::base::status};
 
 mod utils;
+mod controller;
 
 #[tokio::main]
 async fn main() -> Result<(), tokio_postgres::Error>{
@@ -9,5 +12,13 @@ async fn main() -> Result<(), tokio_postgres::Error>{
 
     let connection = connection(&config).await?;
     
+    HttpServer::new(move ||{
+        App::new()
+        .app_data(connection.clone())
+        .service(status)
+    })
+    .bind((config.addresse.as_str(), config.port)).expect("err : no dispnible binding")
+    .run().await;
+
     Ok(())
 }
--- src/utils/database.rs
+++ src/utils/database.rs
@@ -1,23 +1,29 @@
+use std::str::FromStr;
+
 use tokio_postgres::{NoTls, Client};
+use deadpool_postgres::{Manager, ManagerConfig, Pool, RecyclingMethod};
 
 use super::configuration::Configuration;
 
 
 
-pub async fn connection(config : &Configuration) -> Result<Client, tokio_postgres::Error>{
-    let (mut client, connection) =
-        tokio_postgres::connect(config.pg_string.as_str(), NoTls).await?;
-    tokio::spawn(async move {
-        if let Err(err) = connection.await {
-            eprintln!("connection error: {}", err);
-        }
-    });
+pub async fn connection(config : &Configuration) -> Result<Pool, tokio_postgres::Error>{
+    let pg_config = tokio_postgres::Config::from_str(&config.pg_string).expect("Error : not a valid connection string");
+
+    let mgr_config = ManagerConfig {
+        recycling_method: RecyclingMethod::Verified
+    };
+
+    let mgr = Manager::from_config(pg_config, NoTls, mgr_config);
+    let pool = Pool::builder(mgr).max_size(16).build().unwrap();
+
+    let mut client = pool.get().await.unwrap();
 
     if let Err(err) = make_migration(&mut client).await{
         eprintln!("migration error : {}", err);
     }
 
-    Ok(client)
+    Ok(pool)
 }
 
 mod embedded {
